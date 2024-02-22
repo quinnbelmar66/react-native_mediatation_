fba084baad7e2f2bfb00206f9e968d5a8b6c07d9
birkelbachs <stephanieyang2016@gmail.com>
2024-02-22 13:31:09 -0600

    deleted expo/meditation

--- README.md
+++
@@ -1,2 +0,0 @@
-Open this in vscode then CD into the meditation folder, (cd meditation)
-Run "npm ci" in the terminal then "npx expo start"
--- meditation/app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from "expo-router";
-import React from "react";
-
-// Makes the first screen the resources page
-const Index = () => {
-	return <Redirect href="/homepage" />;
-};
-export default Index;
