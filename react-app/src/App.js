9905f739c00b79abcccc14393ec37184e080c195
quinnbelmar66 <107013836+quinnbelmar66@users.noreply.github.com>
2024-03-02 21:13:58 -0600

    buttons do nothing yet

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ButtonTemplate from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ButtonTemplate text={'meditate'} style={{ backgroundColor: "#4e5e84"}} />
+        <ButtonTemplate text={'meditate'} style={{ backgroundColor: "#7180af"}}/>
+        <ButtonTemplate text={'meditate'} style={{ backgroundColor: "#3d4961"}}/>
         <a
           className="App-link"
           href="https://reactjs.org"
--- react-app/src/components/Button.jsx
+++ react-app/src/components/Button.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import {StyleSheet} from 'react';
+import './components.css';
 
 
-function ButtonTemplate(props) {
+// Makes a button with custom style and text
+function ButtonTemplate({text, style}) {
   return (
-    <>
-        <h1> {props.name} </h1>
-    </>
+    <button style={style} className="box">
+        <h1 style={styles.text}> {text} </h1>
+    </button>
+
   );
 }
+
+
+const styles = {
+text: {
+    fontSize: 24,
+    fontWeight: "bold", 
+    textAlign: "center",
+    color: "white",
+},
+}
+
+export default ButtonTemplate;
+
+
